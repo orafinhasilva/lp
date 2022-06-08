2e85fede97edb9bdde07f84494c627f81819aa02
Rafael Silva <88403845+orafinhasilva@users.noreply.github.com>
2022-06-08 17:45:11 -0300

    Add files via upload

---
+++ js/rodape.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let year = date.getFullYear();
+
+$('.rodape').append('<h1><span style="color: #11ff76;">Mestre</span> <strong>do Fut</strong></h1><br><p>Todas as estratégias e investimentos envolvem risco de perda. Nenhuma informação contida neste produto deve ser interpretada como uma garantia de resultados</p><p> Copyright © ' + year + ' - Todos os direitos reservados </p>')
+
